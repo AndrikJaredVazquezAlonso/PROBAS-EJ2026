37bc5c14de2dca71fcc925184102011c45af7aeb
Andrik Jared Vazquez Alonso <andrik.vazqueza@uanl.edu.mx>
2026-03-15 21:44:04 -0600

    Delete PBP43.py

--- PBP43.py
+++
@@ -1,8 +0,0 @@
-#Problema 43: Acumulador de abonos
-suma = 0.0
-while suma < 100000.0:
-    x = float(input("¿Cantidad a abonar? "))
-    if x<0:
-        print("No puede abonar cantidades negativas")
-    else:
-        suma = suma + x
