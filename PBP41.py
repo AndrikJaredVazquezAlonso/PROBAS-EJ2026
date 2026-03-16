5b870ada462e1f44ca28d4716ad9f95c1aa24017
Andrik Jared Vazquez Alonso <andrik.vazqueza@uanl.edu.mx>
2026-03-15 21:43:51 -0600

    Delete PBP41.py

--- PBP41.py
+++
@@ -1,5 +0,0 @@
-#Problema 41: Confirmación de contraseña (sin límite)
-pswrd = input("ingrese su contraseña: ")
-conpswrd = ""
-while conpswrd != pswrd:
-    conpswrd = input("confirme su contraseña: ")
