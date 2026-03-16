cfad4697e95b8f7f978e115c9eaa55784db69f1c
Andrik Jared Vazquez Alonso <andrik.vazqueza@uanl.edu.mx>
2026-03-15 21:43:45 -0600

    Delete PBP40.py

--- PBP40.py
+++
@@ -1,4 +0,0 @@
-#Problema 40: Repetición hasta respuesta específica
-ans = input("¿Cuál es tu lenguaje de programación favorito? ")
-while ans != "Python":
-    ans = input("¿Cuál es tu lenguaje de programación favorito? ")
